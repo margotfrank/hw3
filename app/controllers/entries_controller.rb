145fc21e0edfc59c5b9ef1dfa7bcbbb8e1b86f7d
margotfrank <margot.frank@kellogg.northwestern.edu>
2026-02-23 23:55:12 +0000

    Complete Entries feature with associations and new entry form

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,20 @@
 class EntriesController < ApplicationController
+  def new
+    @place_id = params.fetch("place_id")
+  end
+
+  def create
+    @entry = Entry.new
+    @entry.title = params.fetch("title")
+    @entry.description = params.fetch("description")
+    @entry.occurred_on = params.fetch("occurred_on")
+    @entry.place_id = params.fetch("place_id")
+
+    if @entry.save
+      redirect_to("/places/#{@entry.place_id}")
+    else
+      redirect_to("/entries/new?place_id=#{@entry.place_id}")
+    end
+  end
+
 end
